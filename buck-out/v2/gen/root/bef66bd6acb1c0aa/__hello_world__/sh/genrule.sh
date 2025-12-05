4b3129fd7cb9138c827a2139669d08cdb27504fd
Michael Power <me@dodtsair.com>
2025-12-05 17:13:48 -0600

    something about buck is working

---
+++ buck-out/v2/gen/root/bef66bd6acb1c0aa/__hello_world__/sh/genrule.sh
@@ -0,0 +1,5 @@
+export BUCK_SCRATCH_PATH=../../../../../../../$BUCK_SCRATCH_PATH
+cd buck-out/v2/gen/root/bef66bd6acb1c0aa/__hello_world__/srcs
+mkdir -p ././../out || exit 99
+export TMP=${TMPDIR:-/tmp}
+echo BUILT BY BUCK2> $OUT
